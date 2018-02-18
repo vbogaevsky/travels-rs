46840113f36334f4112c8a17b062689223925157
Vladimir Bogaevsky <vbogaevsky@gmail.com>
2018-02-18 18:45:22 +0300

    scaffolds app

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate iron;
 extern crate router;
-mod lib;
+extern crate rustc_serialize;
 
 use iron::prelude::*;
 use iron::status;
@@ -9,12 +9,13 @@ use router::Router;
 use std::io::Read;
 use rustc_serialize::json;
 
+mod lib;
 
 fn main() {
     Iron::new(|r: &mut Request| {
         println!("{:?}", r);
         let content_type = "application/json".parse::<Mime>().unwrap();
 
-        Ok(Response::with((content_type, status::Ok, hero)))
+        Ok(Response::with((content_type, status::Ok, "")))
     }).http("localhost:7777").unwrap();
 }
