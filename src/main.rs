f1e1ad8138bac87c2c5afd6bbbe0b856888403f4
Vladimir Bogaevsky <vbogaevsky@gmail.com>
2018-03-04 18:50:39 +0300

    attaches four-oh-four error to rocket

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,7 @@ fn not_found() -> Json<Value> {
 
 fn main() {
     rocket::ignite().manage(init_pool())
+                    .catch(errors![not_found])
                     .mount("/users", routes![show_user])
                     // .mount("/users", routes![create_user])
                     .mount("/locations", routes![show_location])
@@ -75,7 +76,7 @@ mod test {
     use lib::schema::users;
     use static_rocket_route_info_for_show_user;
 
-    fn set_user() -> User {
+    fn set_user() -> Result<User, diesel::result::Error> {
         let user = NewUser {
             id: 7004,
             first_name: &String::from("Оксана"),
@@ -86,8 +87,7 @@ mod test {
         };
 
         diesel::insert_into(users::table).values(&user)
-                                         .get_result(&DbConn)
-                                         .expect("Error saving new user")
+                                         .execute(&*DbConn)?
     }
 
     fn prep_rocket(route: &str, handler: Vec<rocket::Route>) -> rocket::Rocket {
@@ -98,7 +98,7 @@ mod test {
     #[test]
     fn get_user() {
         let rocketa      = prep_rocket("/users", routes![show_user]);
-        let user         = set_user();
+        let user         = set_user()?;
         let user_json    = serde_json::to_string(&user);
         let client       = Client::new(rocketa).expect("valid rocket instance");
         let mut response = client.get("/users/1").dispatch();
