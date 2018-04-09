8a9056cda6f63a02dae14b1a5e0d1b80daa8785e
Vladimir Bogaevsky <vbogaevsky@gmail.com>
2018-04-09 23:56:01 +0300

    adds simple avg mark calculation; TBT

--- src/lib/handlers/locations.rs
+++ src/lib/handlers/locations.rs
@@ -5,8 +5,38 @@ use lib::error::Error as ApiError;
 use lib::models::{Location};
 use lib::schema::locations;
 
+#[derive(FromForm, Debug)]
+struct AvgParams {
+    fromDate: Option<i64>,
+    toDate:   Option<i64>,
+    fromAge:  Option<i64>,
+    toAge:    Option<i64>,
+    gender:   Option<String>
+}
+
+#[derive(Serialize, Queryable)]
+struct AvgMark {
+    avg: f64
+}
+
 #[get("/<id>", format = "application/json")]
 fn show(conn: DbConn, id: i64) -> Result<Json<Location>, ApiError> {
     let location = locations::table.find(id).first::<Location>(&*conn)?;
     Ok(Json(location))
+}
+
+#[get("/<id>/avg", format = "application/json")]
+fn avg(conn: DbConn, id: i64) -> Result<Json<AvgMark>, ApiError> {
+    let location = locations::table.find(id).first::<Location>(&*conn)?;
+    let visits = Visit::belonging_to(&location)
+        .load::<Visit>(&*conn)?;
+    let sum: f64 = visits.map (|v| v.mark).iter()
+        .fold(0, |sum, mark| sum + mark);
+    let totalVisits: f64 = visits.count();
+    let avg = sum / totalVisits;
+    Ok(Json(avg))
+}
+
+#[get("/<id>/avg?<params>", format = "application/json")]
+fn queriable_avg(conn: DbConn, id: i64, params: AvgParams) -> Result<Json<AvgMark>, ApiError> {
 }
